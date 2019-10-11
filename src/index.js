f2479a98770142ff8ef80c0997ee300824e03935
nguyen ngoc son <sonnn@innotech-vn.com>
2019-10-11 17:24:14 +0700

    Implement post page

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
-
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 const app = <App />
 
 ReactDOM.render(app, document.getElementById("root"));
--- src/views/experience/create-post.jsx
+++ src/views/experience/create-post.jsx
@@ -1,26 +1,81 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
-
+import "./create-post.css";
 
 class CreatePost extends Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            loadFile: [],
+        }
+        // this.addFile = this.addFile.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log("DIDMOUNT");
     }
 
-    render() {
-        return (
+    changImage = (e) => {
+        console.log(e.currentTarget.files[0]);
+        const fileImage = e.currentTarget.files[0];
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            console.log("Upload");
 
-            <div className="content">
+            const file = {
+                // size: fileImage.size,
+                // type: fileImage.type,
+                data: fileReader.result,
+                isUploading: false
+            }
+            this.addFile(file);
+            // e.target.value = '';
+        }
+        fileReader.onabort = () => {
+
+        }
+        fileReader.onerror = () => {
+
+        }
+        fileReader.readAsDataURL(fileImage);
+    }
+
+    addFile(file) {
+    
+        this.setState((preState) => ({ loadFile: [...preState.loadFile, file] }))
+    }
+
+    removeFile = (file) => {
+        this.setState({loadFile: []});
+    }
+
+
+    render() {
+        const { loadFile } = this.state;
+        return (
+            <div className="content">  
                 <Row>
-                    <Col lg={3} sm={6}>
-                        askldjalksdjlkasjkld
-                        {/* <button type="button" className="btn btn-primary" onClick={this.createPost}>Tạo bài viết</button> */}
-                        {/* <Link to='/admin/dashboard/post' className="btn btn-info mb-10">Create Product</Link> */}
+                    <Col lg={3} sm={3}>
+                        <div className="poster">
+                            <i className="fa fa-upload Icon"></i>
+                            <input type="file" onChange={this.changImage} onClick={(event) => event.target.value = ''} />
+                        </div>
+                    </Col>
+
+                    <Col lg={3} sm={3}>
+                        <div >
+                            {loadFile.map((file, idx) => {
+                                return <div className="file" key={idx}>
+                                    <div className="container">
+                                        <span className="remove-btn" onClick={this.removeFile}>
+                                            <i className="fa fa-minus remove"></i>
+                                        </span>
+                                    </div>
+                                    <img className="image" src={file.data} />
+
+                                </div>
+                            })}
+                        </div>
                     </Col>
 
                 </Row>
